77bacf37a7dbe2c6014539d5265495944c8cefe1
interysy <2554268w@student.gla.ac.uk>
2023-10-05 21:42:20 +0100

    feat(navbar): added mobile responsiveness of the navbar
    done by detecting window size and on click
    
    related to #43

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , ViewChild, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  @ViewChild('mobileMenu', {static: true}) mobileMenu: ElementRef<HTMLDivElement>;
+
   // will be exported onto a config file - added as an issue to backlog - #43
   algorithms = [
     "Brute Force",
@@ -14,4 +16,23 @@ export class NavbarComponent {
     "Knuth-Morris-Pratt",
   ];
 
+  /**
+   * A class to toggle the mobile menu on a smaller screen
+   */
+  toggleMobileMenu() {
+    this.mobileMenu.nativeElement.classList.toggle('hidden');
+  }
+
+  /**
+   * A class to hide the mobile menu when the window is resized,
+   * avoids bug where the mobile menu is still visible on a larger screen if not closed
+   */
+  @HostListener('window:resize')
+  onResize() {
+    const windowWidth = window.innerWidth;
+
+    if (windowWidth > 768) {
+      this.mobileMenu.nativeElement.classList.add('hidden');
+    }
+  }
 }
