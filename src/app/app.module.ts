1794fd08e66b99fa9fd46a0253c193e1e3e079cc
interysy <2554268w@student.gla.ac.uk>
2023-10-05 19:40:32 +0100

    refactor(app-module): import all page components to application
    related to #38 , #39

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home/home.page';
+import { AboutPageComponent } from './pages/about/about.page';
+import { AlgorithmVisualiserPageComponent } from './pages/algorithm-visualiser/algorithm-visualiser.page';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    AboutPageComponent,
+    AlgorithmVisualiserPageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
