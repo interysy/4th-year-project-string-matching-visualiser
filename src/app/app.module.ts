5028af37d7b4b02fe92e853b6d52d6c340fd8789
interysy <2554268w@student.gla.ac.uk>
2023-10-05 21:44:14 +0100

    feat(app-module): add icons used in navbar and remove boilerplate html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faGithub , faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faCompass } from '@fortawesome/free-solid-svg-icons';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,19 +11,30 @@ import { AboutPageComponent } from './pages/about/about.page';
 import { AlgorithmVisualiserPageComponent } from './pages/algorithm-visualiser/algorithm-visualiser.page';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     AboutPageComponent,
     AlgorithmVisualiserPageComponent,
-    NavbarComponent
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(
+      faGithub,
+      faLinkedin,
+      faCompass
+    );
+  }
+}
