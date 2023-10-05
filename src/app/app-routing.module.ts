ffe6eb6c2336236e10591aac9c2c0786d5769b69
interysy <2554268w@student.gla.ac.uk>
2023-10-05 21:54:56 +0100

    feat(set-up-routing): set up routes for home, about and alg-visualiser
    while at it, I have also added the created navbar to each page
    
    related to #44

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home/home.page';
+import { AboutPageComponent } from './pages/about/about.page';
+import { AlgorithmVisualiserPageComponent } from './pages/algorithm-visualiser/algorithm-visualiser.page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomePageComponent },
+  { path : "about" , component : AboutPageComponent },
+  { path : "algorithm-visualiser" , component : AlgorithmVisualiserPageComponent},
+  { path: "**", redirectTo: "" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
