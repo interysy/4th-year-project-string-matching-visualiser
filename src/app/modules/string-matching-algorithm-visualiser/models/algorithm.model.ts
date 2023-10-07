40eec8587528b3c1695f26ef9bfa1368059f7487
interysy <2554268w@student.gla.ac.uk>
2023-10-07 15:49:27 +0100

    feat(algorithm): create algorithm abstract class
    abstract class is to be extended by each concrete algorithm to be implemented including : brute force , KMP and BM
    
    what is included:
    - steps array
    - function to work out the steps (runs algorithm)
    - function to append a step
    
    related to #51

--- src/app/modules/string-matching-algorithm-visualiser/models/algorithm.model.ts
+++ src/app/modules/string-matching-algorithm-visualiser/models/algorithm.model.ts
@@ -1,2 +1,16 @@
+import { Injectable } from "@angular/core";
+import { AlgorithmStep } from "./algorithm-step.model";
+
+@Injectable({
+    providedIn: 'root'
+})
 export abstract class StringMatchingAlgorithm {
+
+    steps : AlgorithmStep[] = [];
+
+    abstract workOutSteps(text : string , pattern : string) : number;
+
+    public addStep(algorithmStep : AlgorithmStep) {
+        this.steps.push(algorithmStep);
+    }
 }
