9f3b4a16c04bbc64e142d0a1bccdf71f5ce79b81
interysy <2554268w@student.gla.ac.uk>
2023-10-12 19:48:34 +0100

    feat(routing): fix links for each algorithm
    changes:
    - make links from the config file
    - send relevant service information for injection upon clicking on a link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home/home.page';
 import { AboutPageComponent } from './pages/about/about.page';
 import { AlgorithmVisualiserPageComponent } from './pages/algorithm-visualiser/algorithm-visualiser.page';
-import { implementedAlgorithmsGuard } from './modules/string-matching-algorithm-visualiser/guards/implemented-algorithms.guard';
+import { environment } from 'environment';
 
+
+const algorithmVisualiserRoutes: Routes = environment.supportedAlgorithms.map(algorithm => {
+  return {path : "algorithm-visualiser/" + algorithm.urlParam ,  pathMatch: 'full',  component : AlgorithmVisualiserPageComponent , data : {requiredService : algorithm.requiredService }}
+});
 const routes: Routes = [
   { path: "", component: HomePageComponent },
-  { path : "about" , component : AboutPageComponent },
-  { path : "algorithm-visualiser/:algorithm" , component : AlgorithmVisualiserPageComponent , canActivate : [implementedAlgorithmsGuard]},
-  // { path: "**", redirectTo: "" },
+  { path : "about" , pathMatch: 'full', component : AboutPageComponent },
+  ...algorithmVisualiserRoutes,
+  { path: "**", redirectTo: "" },
 ];
 
 @NgModule({
--- src/app/modules/string-matching-algorithm-visualiser/string-matching-algorithm-visualiser.module.ts
+++ src/app/modules/string-matching-algorithm-visualiser/string-matching-algorithm-visualiser.module.ts
@@ -1,14 +1,10 @@
-import { Injector, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlgorithmVisualiserComponent } from './components/algorithm-visualiser/algorithm-visualiser.component';
 import { PseudocodeVisualiserComponent } from './components/pseudocode-visualiser/pseudocode-visualiser.component';
 import { VariableVisualiserComponent } from './components/variable-visualiser/variable-visualiser.component';
 import { CommandVisualiserComponent } from './components/command-visualiser/command-visualiser.component';
 import { FormsModule } from '@angular/forms';
-import { BruteForceAlgorithm } from './algorithms/brute-force/brute-force.algorithm';
-import { ActivatedRoute } from '@angular/router';
-import { StringMatchingAlgorithm } from './models/algorithm.model';
-
 
 @NgModule({
   declarations: [
@@ -26,21 +22,6 @@ import { StringMatchingAlgorithm } from './models/algorithm.model';
     PseudocodeVisualiserComponent,
     VariableVisualiserComponent,
     CommandVisualiserComponent,
-  ],
-  providers:[
-    {
-      provide: StringMatchingAlgorithm,
-      useFactory: (activatedRoute: ActivatedRoute, injector:Injector) => {
-            const param = activatedRoute.snapshot.queryParams['algorithm'];
-            console.log(param);
-            switch (param) {
-              case "bruteForce": return injector.get(BruteForceAlgorithm)
-              default : throw new Error(`No service defined for the algorithm: "${param}"`);
-            }
-
-      },
-      deps:[ActivatedRoute, Injector]
-    }
   ],
 })
 export class StringMatchingAlgorithmVisualiserModule { }
