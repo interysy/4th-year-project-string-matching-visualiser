0ab8838265ee9c131f7db0725e7851a667c67691
interysy <2554268w@student.gla.ac.uk>
2023-10-15 21:08:38 +0100

    feat(algorithm-step-constant): add step types
    this will allow for checking whether there has been a match or not and hence redraw the animation

---
+++ src/app/modules/string-matching-algorithm-visualiser/constants/algorithm-step-model.constant.ts
@@ -0,0 +1,5 @@
+export abstract class AlgorithmStepTypeConstants {
+    static readonly MISMATCH = "MISMATCH";
+    static readonly MATCH = "MATCH";
+    static readonly PROCESSING = "PROCESSING";
+}
