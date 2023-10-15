0cad9c76dcd6c65bb881b41d8a4bacb491747bc7
interysy <2554268w@student.gla.ac.uk>
2023-10-15 11:52:31 +0100

    feat(algorithm-visualiser): add p5js to relevant component
    algorithm visualiser component will need to actually draw the visualisation on the p5js canvas. the component so far only instantiates the service and then draws a circle. There is also a method to draw more circles as means of testing.

--- src/app/modules/string-matching-algorithm-visualiser/components/algorithm-visualiser/algorithm-visualiser.component.ts
+++ src/app/modules/string-matching-algorithm-visualiser/components/algorithm-visualiser/algorithm-visualiser.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { AlgorithmProgressService } from '../../services/algorithm-progress.service';
 import { Subject , debounceTime , distinctUntilChanged } from 'rxjs';
+import { P5jsDrawService } from '../../services/p5js-draw-service.service';
 
 @Component({
   selector: 'app-algorithm-visualiser-animation',
   templateUrl: './algorithm-visualiser.component.html',
   styleUrls: ['./algorithm-visualiser.component.css']
 })
+
 export class AlgorithmVisualiserComponent {
 
   text = "The fox jumped over the lazy dog";
   pattern = "lazy";
+
+  where = 200;
+
+  @ViewChild('canvasDemo', {static: true})
+  canvas: ElementRef<HTMLCanvasElement>;
 
   stringSettings = false;
 
@@ -19,8 +26,14 @@ export class AlgorithmVisualiserComponent {
   textChanged: Subject<string> = new Subject<string>();
   patternChanged : Subject<string> = new Subject<string>();
 
-  constructor(private algorithmProgressService : AlgorithmProgressService ) {
+
+  constructor(private algorithmProgressService : AlgorithmProgressService , private p5DrawService : P5jsDrawService) {
     this.algorithmProgressService.setTextAndPattern(this.text , this.pattern);
+    // console.log(this.canvas);
+    // if (document.getElementById("canvas")) {
+    //   console.log("Initialising p5js");
+    //   p5DrawService.initialiseP5(document.getElementById("canvas"));
+    // }
 
     this.textChanged
     .pipe(debounceTime(1000), distinctUntilChanged())
@@ -35,12 +48,22 @@ export class AlgorithmVisualiserComponent {
     });
   }
 
+  ngAfterViewInit() {
+    console.log(this.canvas);
+    this.p5DrawService.initiate(this.canvas.nativeElement);
+  }
+
   protected sendTextToService() {
     this.textChanged.next(this.text)
   }
 
   protected sendPatternToService() {
     this.patternChanged.next(this.pattern)
+  }
+
+  drawMore(offset : number) {
+    this.where += offset
+    this.p5DrawService.drawMore(this.where);
   }
 
 }
