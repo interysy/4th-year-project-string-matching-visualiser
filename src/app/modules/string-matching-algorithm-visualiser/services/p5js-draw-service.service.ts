1f31c4d8c0a443a9be880747003b646a65aa5173
interysy <2554268w@student.gla.ac.uk>
2023-10-16 22:35:57 +0100

    feat(p5drawservice): add drawing functions
    add functions to be used by the visualiser for drawing , this includes:
    - drawing text
    - colouring squares
    - redrawing text at new offset
    
    related to #63

--- src/app/modules/string-matching-algorithm-visualiser/services/p5js-draw-service.service.ts
+++ src/app/modules/string-matching-algorithm-visualiser/services/p5js-draw-service.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import * as p5 from 'p5';
 import { P5JSInvoker } from '../models/p5-jsinvoker';
-import { text } from '@fortawesome/fontawesome-svg-core';
+import { MatchingAlgorithmColourConstants } from '../constants/matching-algorithm-colours.constant';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +11,8 @@ export class P5jsDrawService extends P5JSInvoker {
   private readonly SquareSize = 20
 
   private textWidth : number;
+
+  private oldOffset = 0;
 
   initiate(canvas : HTMLElement | null , width : number , height : number , text : string , pattern : string) {
 
@@ -32,29 +34,46 @@ export class P5jsDrawService extends P5JSInvoker {
   }
 
 
-  private drawText(text : string) {
-
+  private drawText(text : string , fill = "#ffffff" , alreadyMatched : number[] = []) {
+    const oldFill = fill;
     [...text].forEach((char , index) => {
+      if (alreadyMatched.indexOf(index) !== -1) {
+        fill = MatchingAlgorithmColourConstants.MATCH;
+      } else {
+        fill = oldFill;
+      }
+
       this.p5.text(index , index * 20 + 10 , 10);
+      this.p5.fill(fill);
       this.p5.rect(index * 20 + 5 , 15 , 20 , 20);
+      this.p5.fill("#000000");
       this.p5.text(char , index * 20  + 10 , 30);
     })
   }
 
-  private drawPattern(pattern : string) {
+  private drawPattern(pattern : string , fill = "#ffffff" , patternOffset = 0 , alreadyMatched : number[] = []) {
+    const oldFill = fill;
     [...pattern].forEach((char , index) => {
-      this.p5.rect(index * 20 + 5 , 40 , 20 , 20);
-      this.p5.text(char , index * 20 + 10 , 55);
+      if (alreadyMatched.indexOf(index) !== -1) {
+        fill = MatchingAlgorithmColourConstants.MATCH;
+      } else {
+        fill = oldFill;
+      }
+
+      this.p5.fill(fill);
+      this.p5.rect(index * 20 + 5  + patternOffset , 40 , 20 , 20);
+      this.p5.fill("#000000");
+      this.p5.text(char , index * 20 + 10 + patternOffset , 55);
     });
   }
 
-  drawTextAndPattern(text : string , pattern : string) {
+  drawTextAndPattern(text : string , pattern : string , patternOffset = 0 , alreadyMatchedText : number[] = [] , alreadyMatchedPattern : number[] = []) {
     this.p5.background(255);
 
     this.workOutTextWidth(text);
     this.centraliseDrawing(this.p5.width , this.p5.height);
-    this.drawText(text);
-    this.drawPattern(pattern);
+    this.drawText(text , "#ffffff",alreadyMatchedText);
+    this.drawPattern(pattern , "#ffffff", patternOffset , alreadyMatchedPattern);
   }
 
   workOutTextWidth(text : string) : void {
@@ -65,4 +84,37 @@ export class P5jsDrawService extends P5JSInvoker {
     const centralCoordinate = (canvasWidth - this.textWidth)/2;
     this.p5.translate(centralCoordinate , canvasHeight / 2);
   }
+
+
+  highlightTextAndPattern(text : string , pattern : string , textColor : boolean , patternColor : boolean ) {
+    this.p5.background(255);
+    this.workOutTextWidth(text);
+    this.centraliseDrawing(this.p5.width , this.p5.height);
+
+    if (textColor) {
+      this.drawText(text , MatchingAlgorithmColourConstants.CHECKING);
+    } else {
+      this.drawText(text);
+    }
+
+    if (patternColor) {
+      this.drawPattern(pattern ,MatchingAlgorithmColourConstants.CHECKING , this.oldOffset);
+    } else {
+      this.drawPattern(pattern);
+    }
+
+  }
+
+  movePattern(textIndex : number , text : string , pattern : string) {
+    const offset = textIndex *  this.SquareSize;
+    this.oldOffset = offset;
+    this.drawTextAndPattern(text , pattern , offset);
+  }
+
+  highlightSpecificSquares(color : string , textIndex : number , patternIndex : number , alreadyMatchedPattern : number[] , alreadyMatchedText : number[] , text : string , pattern : string) {
+    alreadyMatchedText = alreadyMatchedText.map(textIndex => textIndex - 1);
+    alreadyMatchedPattern = alreadyMatchedPattern.map(patternIndex => patternIndex - 1);
+    this.drawTextAndPattern(text , pattern , this.oldOffset , alreadyMatchedText , alreadyMatchedPattern);
+  }
+
 }
