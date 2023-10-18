4bcc1dd0eab3fbf367edef85e7461df154d500e9
interysy <2554268w@student.gla.ac.uk>
2023-10-18 22:56:26 +0100

    feat(algorithm-visualiser): utilise drawing functions
    changes:
    - call relevant function when progress service notifies of step change
    - add dummy functions for testing
    
    related to #73

--- src/app/modules/string-matching-algorithm-visualiser/components/algorithm-visualiser/algorithm-visualiser.component.ts
+++ src/app/modules/string-matching-algorithm-visualiser/components/algorithm-visualiser/algorithm-visualiser.component.ts
@@ -1,13 +1,111 @@
-import { Component, OnInit } from '@angular/core';
-import { StringMatchingAlgorithm } from '../../models/algorithm.model';
-import { BruteForceAlgorithm } from '../../algorithms/brute-force.algorithm';
-import { PlaybackServiceService } from '../../services/playback-service.service';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { AlgorithmProgressService } from '../../services/algorithm-progress.service';
+import { Subject , debounceTime , distinctUntilChanged } from 'rxjs';
+import { P5jsDrawService } from '../../services/p5js-draw.service';
+import { Letter } from '../../models/letter.model';
+import { AlgorithmStep } from '../../models/algorithm-step.model';
+
 
 @Component({
   selector: 'app-algorithm-visualiser-animation',
   templateUrl: './algorithm-visualiser.component.html',
   styleUrls: ['./algorithm-visualiser.component.css']
 })
-export class AlgorithmVisualiserComponent {
+
+export class AlgorithmVisualiserComponent implements AfterViewInit {
+
+  text = "The fox jumped over the lazy dog";
+  pattern = "lazy";
+
+  @ViewChild('canvas', {static: true})
+  canvas: ElementRef<HTMLCanvasElement>;
+
+  stringSettings = true;
+
+  private readonly Debounce = 1000;
+
+  textChanged: Subject<string> = new Subject<string>();
+  patternChanged : Subject<string> = new Subject<string>();
+
+  constructor(private algorithmProgressService : AlgorithmProgressService , private p5jsDrawService : P5jsDrawService ) {
+    this.algorithmProgressService.setTextAndPattern(this.text , this.pattern);
+
+    this.textChanged
+    .pipe(debounceTime(this.Debounce), distinctUntilChanged())
+    .subscribe(_ => {
+       algorithmProgressService.setText = this.text;
+
+       this.p5jsDrawService.drawTextAndPattern(this.stringToLetterObject(this.text , "#ffffff" , 1), this.stringToLetterObject(this.pattern , "#ffffff" , 1) , 0 );
+    });
+
+    this.patternChanged
+    .pipe(debounceTime(this.Debounce), distinctUntilChanged())
+    .subscribe(_ => {
+       algorithmProgressService.setPattern = this.pattern;
+       this.p5jsDrawService.drawTextAndPattern(this.stringToLetterObject(this.text , "#ffffff" , 1) , this.stringToLetterObject(this.pattern,"#ffffff" , 1) , 0);
+    });
+
+    this.algorithmProgressService.notifier.subscribe((_) => {
+      const step = this.algorithmProgressService.stepGetter;
+      this.p5jsDrawService.drawTextAndPattern(step.lettersInText , step.lettersInPattern , step.patternOffset);
+    });
+  }
+
+
+  ngAfterViewInit() {
+    const canvasWidth = this.canvas.nativeElement.offsetWidth;
+
+    // need to get height
+
+    this.p5jsDrawService.initiate(this.canvas.nativeElement , canvasWidth, 200 );
+
+    // force change
+    this.textChanged.next(this.text);
+  }
+
+
+  protected sendTextToService() {
+    this.textChanged.next(this.text)
+  }
+
+  protected sendPatternToService() {
+    this.patternChanged.next(this.pattern)
+  }
+
+  private stringToLetterObject(stringToChange : string , colour : string , strokeWeight : number ) : Letter[] {
+    return stringToChange.split('').map((letter , index) => {
+      const letterObj = new Letter();
+      letterObj.index = index;
+      letterObj.letter = letter;
+      letterObj.colour = colour;
+      letterObj.strokeWeight = strokeWeight;
+
+      return letterObj;
+    });
+  }
+
+  // @HostListener('window:resize')
+  // protected onResize() {
+  //   const canvasHeigth = this.canvas.nativeElement.offsetHeight;
+  //   const canvasWidth = this.canvas.nativeElement.offsetWidth;
+
+  //   //this.p5DrawService.resizeCanvas(canvasWidth, canvasHeigth , this.text , this.pattern);
+  // }
+
+  previousStep() {
+    this.algorithmProgressService.goToPreviousStep();
+  }
+
+  play() {
+    this.algorithmProgressService.play();
+  }
+
+  nextStep() {
+    this.algorithmProgressService.moveToNextStep();
+  }
+
+  reset() {
+    this.algorithmProgressService.reset();
+  }
 
 }
