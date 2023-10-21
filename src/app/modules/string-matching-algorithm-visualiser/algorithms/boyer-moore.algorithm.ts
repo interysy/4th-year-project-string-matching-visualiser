960fdba1e66666a7e44006e6b2747b8b5abebfdf
interysy <2554268w@student.gla.ac.uk>
2023-10-21 20:47:14 +0100

    feat(boyer-moore): implement boyer moore visualisations
    changes:
    - implementation of on match step
    - implementation of mismatch step
    - reuse of addWhileLoopStep and addCheckStep from brute force
    
    current solution will need cleaning up!
    
    related to #91

--- src/app/modules/string-matching-algorithm-visualiser/algorithms/boyer-moore.algorithm.ts
+++ src/app/modules/string-matching-algorithm-visualiser/algorithms/boyer-moore.algorithm.ts
@@ -5,6 +5,7 @@ import { Letter } from "../models/letter.model";
 import { AlgorithmStep } from "../models/algorithm-step.model";
 import { MatchingAlgorithmColourConstants } from "../constants/matching-algorithm-colours.constant";
 import { BoyerMooreAdditionalVariables } from "../models/boyer-moore-additional-variables.model";
+import { LetterBuilder } from "../model-builders/letter.builder";
 
 
 @Injectable({
@@ -14,6 +15,7 @@ export class BoyerMooreAlgorithm extends StringMatchingAlgorithm {
 
     startingPoint : number;
     private readonly algorithmStepBuilder: AlgorithmStepBuilder = new AlgorithmStepBuilder();
+    private readonly letterBuilder : LetterBuilder = new LetterBuilder();
     previousStep : AlgorithmStep;
     text : string;
     pattern : string;
@@ -31,14 +33,20 @@ export class BoyerMooreAlgorithm extends StringMatchingAlgorithm {
 
         const lastOccurance = this.setUpLastOccuranceDictionary(pattern);
 
+        console.log("Entering while loop");
         while (startingPoint <= (textLength - patternLength) && patternIndex >= 0) {
+            this.addWhileLoopStep(textIndex , patternIndex);
+            this.addCheckStep(textIndex , patternIndex);
             if (text.charAt(textIndex) === pattern.charAt(patternIndex)) {
                 textIndex--;
                 patternIndex--;
+                this.addMatchStep(textIndex , patternIndex);
             } else {
-                startingPoint += Math.max(1, patternIndex - lastOccurance[text.charAt(textIndex)]);
-                textIndex += textLength - Math.min(patternIndex , 1 + lastOccurance[text.charAt(textIndex)]);
-                patternIndex = textLength - 1;
+                const occurance = (lastOccurance[text.charAt(textIndex)] === undefined) ? 0  : lastOccurance[text.charAt(textIndex)];
+                startingPoint += Math.max(1, patternIndex - occurance);
+                textIndex += (patternLength - Math.min(patternIndex , 1 + occurance));
+                patternIndex = patternLength - 1;
+                this.addMismatchStep(startingPoint , startingPoint , patternIndex);
             }
         }
 
@@ -51,6 +59,7 @@ export class BoyerMooreAlgorithm extends StringMatchingAlgorithm {
             if (lastOccuranceDictionary[character] === undefined) lastOccuranceDictionary[character] = pattern.lastIndexOf(character);
         });
 
+        console.log(lastOccuranceDictionary);
         return lastOccuranceDictionary;
     }
 
@@ -92,6 +101,139 @@ export class BoyerMooreAlgorithm extends StringMatchingAlgorithm {
             this.algorithmStepBuilder.setDefaults();
             this.previousStep = step;
         });
+    }
+
+
+    addWhileLoopStep(textIndex : number , patternIndex : number) : void {
+
+        this.algorithmStepBuilder.setPseudocodeLine = 9;
+        this.algorithmStepBuilder.setPatternIndex = patternIndex;
+        this.algorithmStepBuilder.setTextIndex = textIndex;
+        this.algorithmStepBuilder.setPatternOffset = this.previousStep.patternOffset;
+        this.algorithmStepBuilder.setLettersInText = this.previousStep.lettersInText;
+        this.algorithmStepBuilder.setLettersInPattern = this.previousStep.lettersInPattern;
+        this.algorithmStepBuilder.setCommand = "Looping through the pattern and text looking for a match";
+        const currentStep = this.algorithmStepBuilder.build();
+        this.addStep(currentStep);
+        this.algorithmStepBuilder.setDefaults();
+
+        this.previousStep = currentStep;
+    }
+
+    addCheckStep(textIndex : number , patternIndex : number) : void {
+
+        this.algorithmStepBuilder.setPseudocodeLine = 10;
+        this.algorithmStepBuilder.setPatternIndex = patternIndex;
+        this.algorithmStepBuilder.setTextIndex = textIndex;
+        this.algorithmStepBuilder.setPatternOffset = this.previousStep.patternOffset;
+
+        this.letterBuilder.setIndex = textIndex;
+        this.letterBuilder.setLetter = this.text.charAt(textIndex);
+        this.letterBuilder.setColor = MatchingAlgorithmColourConstants.CHECKING;
+        this.letterBuilder.setStrokeWeight = 4;
+        this.algorithmStepBuilder.setLettersInText = this.replaceLetter(this.previousStep.lettersInText, this.letterBuilder.build());
+
+        this.letterBuilder.setIndex = patternIndex;
+        this.letterBuilder.setLetter = this.pattern.charAt(patternIndex);
+        this.algorithmStepBuilder.setLettersInPattern = this.replaceLetter(this.previousStep.lettersInPattern, this.letterBuilder.build());
+
+        this.algorithmStepBuilder.setCommand = "Checking if the 2 characters match";
+
+        const step = this.algorithmStepBuilder.build();
+        this.addStep(step);
+        this.previousStep = step;
+        this.algorithmStepBuilder.setDefaults();
+        this.letterBuilder.setDefaults();
+
+    }
+
+    addMatchStep(textIndex : number , patternIndex : number) {
+
+        this.algorithmStepBuilder.setPseudocodeLine = 11;
+        this.algorithmStepBuilder.setPatternIndex = this.previousStep.patternIndex;
+        this.algorithmStepBuilder.setTextIndex = textIndex;
+        this.algorithmStepBuilder.setPatternOffset = this.previousStep.patternOffset;
+        this.algorithmStepBuilder.setCommand = "Found a character match - move one character left in text";
+
+
+        this.letterBuilder.setIndex = this.previousStep.patternIndex;
+        this.letterBuilder.setLetter = this.pattern.charAt(this.previousStep.patternIndex);
+        this.letterBuilder.setColor = MatchingAlgorithmColourConstants.MATCH;
+        this.letterBuilder.setStrokeWeight = 4;
+
+        this.algorithmStepBuilder.setLettersInPattern = this.replaceLetter(this.previousStep.lettersInPattern, this.letterBuilder.build());
+
+        this.letterBuilder.setIndex = this.previousStep.textIndex;
+        this.letterBuilder.setLetter = this.text.charAt(this.previousStep.textIndex);
+        this.letterBuilder.setColor = MatchingAlgorithmColourConstants.MATCH;
+        this.letterBuilder.setStrokeWeight = 4;
+
+        this.algorithmStepBuilder.setLettersInText = this.replaceLetter(this.previousStep.lettersInText, this.letterBuilder.build());
+
+        let step = this.algorithmStepBuilder.build();
+        this.addStep(step);
+        this.previousStep = step;
+
+        this.algorithmStepBuilder.setPseudocodeLine = 11;
+        this.algorithmStepBuilder.setPatternIndex = patternIndex;
+        this.algorithmStepBuilder.setCommand = "Move one character left in pattern";
+
+        step = this.algorithmStepBuilder.build();
+        this.addStep(step);
+        this.previousStep = step;
+        this.algorithmStepBuilder.setDefaults();
+        this.letterBuilder.setDefaults();
+        this.previousStep = step;
+    }
+
+    addMismatchStep(startingPoint : number , textIndex : number , patternIndex : number) {
+
+        this.algorithmStepBuilder.setPseudocodeLine = 13;
+        this.algorithmStepBuilder.setPatternIndex = this.previousStep.patternIndex;
+        this.algorithmStepBuilder.setTextIndex = this.previousStep.textIndex;
+        this.algorithmStepBuilder.setPatternOffset = this.previousStep.patternOffset;
+        this.algorithmStepBuilder.setCommand = "No character match found, enter the else block";
+
+        this.letterBuilder.setIndex = this.previousStep.patternIndex;
+        this.letterBuilder.setLetter = this.pattern.charAt(this.previousStep.patternIndex);
+        this.letterBuilder.setColor = MatchingAlgorithmColourConstants.MISMATCH;
+        this.letterBuilder.setStrokeWeight = 4;
+
+        this.algorithmStepBuilder.setLettersInPattern = this.replaceLetter(this.highlightEntireLine(this.pattern,MatchingAlgorithmColourConstants.DEFAULT, 1), this.letterBuilder.build());
+
+        this.letterBuilder.setIndex = this.previousStep.textIndex;
+        this.letterBuilder.setLetter = this.text.charAt(this.previousStep.textIndex);
+
+        this.algorithmStepBuilder.setLettersInText = this.replaceLetter(this.highlightEntireLine(this.text,MatchingAlgorithmColourConstants.DEFAULT, 1), this.letterBuilder.build());
+
+        let step = this.algorithmStepBuilder.build();
+        this.addStep(step);
+        this.previousStep = step;
+
+        this.algorithmStepBuilder.setPseudocodeLine = 14;
+        this.algorithmStepBuilder.setPatternIndex = 0;
+        this.algorithmStepBuilder.setCommand = "Set starting point to lastOccurance of character in text or the next character in text";
+        this.algorithmStepBuilder.setLettersInPattern = this.highlightEntireLine(this.pattern , MatchingAlgorithmColourConstants.DEFAULT , 1);
+        this.algorithmStepBuilder.setLettersInText = this.highlightEntireLine(this.text , MatchingAlgorithmColourConstants.DEFAULT , 1);
+
+        step = this.algorithmStepBuilder.build();
+        this.addStep(step);
+        this.previousStep = step;
+
+        this.algorithmStepBuilder.setPseudocodeLine = 15;
+        this.algorithmStepBuilder.setCommand = "Set text index to last in text when pattern will be shifted to the right";
+
+        step = this.algorithmStepBuilder.build();
+        this.addStep(step);
+        this.previousStep = step;
+
+        this.algorithmStepBuilder.setPseudocodeLine = 16;
+        this.algorithmStepBuilder.setCommand = "Increment starting point of comparison to next element of text";
+        this.algorithmStepBuilder.setPatternOffset = textIndex;
+
+        step = this.algorithmStepBuilder.build();
+        this.addStep(step);
+        this.previousStep = step;
     }
 
     highlightEntireLine(stringToHighlight : string , colour : MatchingAlgorithmColourConstants, weight : number) : Letter[] {
