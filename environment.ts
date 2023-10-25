87c5010420b8ed10a0d81a122c5ad6765686ea75
interysy <2554268w@student.gla.ac.uk>
2023-10-25 10:03:32 +0100

    feat(environment): changing env for better dependency injection
    changes:
    - sluggified algorithm name property
    - passing a concrete class to be used with that algorithm as property
    
    related to #93

--- environment.ts
+++ environment.ts
@@ -1,7 +1,10 @@
+import { BoyerMooreAlgorithm } from "src/app/modules/string-matching-algorithm-visualiser/algorithms/boyer-moore.algorithm";
+import { BruteForceAlgorithm } from "src/app/modules/string-matching-algorithm-visualiser/algorithms/brute-force.algorithm";
+
 export const environment = {
   supportedAlgorithms: [
-    {name : "Brute Force" , urlParam : "bruteForce" , requiredService : "BruteForceAlgorithm"},
-    {name : "Boyer-Moore" , urlParam : "boyerMoore" , requiredService : "BoyerMooreAlgorithm"},
-    {name : "Knuth-Morris-Pratt" , urlParam : "knuthMorrisPratt" , requiredService : "KnuthMorrisPrattAlgorithm"},
+    {name : "Brute Force" , nameSlug : "brute-force" , urlParam : "bruteForce" , requiredService : BruteForceAlgorithm},
+    {name : "Boyer Moore" ,nameSlug : "boyer-moore" , urlParam : "boyerMoore" , requiredService : BoyerMooreAlgorithm},
+    {name : "Knuth-Morris-Pratt" , nameSlug : "knuth-morris-pratt" , urlParam : "knuthMorrisPratt" , requiredService : null},
   ]
 };
