244b8d70cd47b2dd8a8d9f3faf6ef076ece56c07
interysy <2554268w@student.gla.ac.uk>
2023-10-31 08:56:15 +0000

    feat(decorators): add base decorator class to be extended by decorators
    necessary for decorators from earlier commit to work, as they extend this class

---
+++ src/app/modules/string-matching-algorithm-visualiser/models/drawer-step.decorator.ts
@@ -0,0 +1,16 @@
+import * as p5 from "p5";
+import { StringMatchingAlgorithmToDraw } from "./algorithm-draw.model";
+import { AlgorithmStep } from "./algorithm-step.model";
+
+
+export abstract class DrawStepDecorator implements StringMatchingAlgorithmToDraw {
+    earlierDrawer : StringMatchingAlgorithmToDraw;
+
+    constructor(earlierDrawer : StringMatchingAlgorithmToDraw) {
+        this.earlierDrawer = earlierDrawer;
+    }
+
+    draw(p : p5 , step : AlgorithmStep) {
+        this.earlierDrawer.draw(p , step);
+    }
+}
