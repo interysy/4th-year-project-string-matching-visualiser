ccfe2a2ab678ac491431da80138faea866bd5c4a
interysy <2554268w@student.gla.ac.uk>
2023-10-31 08:55:23 +0000

    feat(decorators): add decorators to draw text+pattern and last occurance
    an algorithm will be decorated with these (can be both or none or one) depending on algorithm necessities

---
+++ src/app/modules/string-matching-algorithm-visualiser/drawers/text-pattern.drawer.decorator.ts
@@ -0,0 +1,25 @@
+import * as p5 from "p5";
+import { StringMatchingAlgorithmToDraw } from "../models/algorithm-draw.model";
+import { DrawStepDecorator } from "../models/drawer-step.decorator";
+import { AlgorithmStep } from "../models/algorithm-step.model";
+import { P5jsDrawService } from "../services/p5js-draw.service";
+import { Injector } from "@angular/core";
+
+
+export class TextAndPatternDrawer extends DrawStepDecorator {
+
+    private p5jsDrawService : P5jsDrawService;
+    constructor(earlierDrawer : StringMatchingAlgorithmToDraw) {
+        super(earlierDrawer);
+        this.p5jsDrawService = Injector.create({providers: [{provide: P5jsDrawService, deps: []}]}).get(P5jsDrawService);
+
+    }
+
+    override draw(p : p5 , step : AlgorithmStep) : void {
+        this.earlierDrawer.draw(p , step);
+        const textLettersToDrawFromStep = step.lettersInText;
+        const patternLettersToDrawFromStep = step.lettersInPattern;
+        const patternOffsetFromStep = step.patternOffset;
+        this.p5jsDrawService.drawTextAndPattern(p , textLettersToDrawFromStep , patternLettersToDrawFromStep , patternOffsetFromStep);
+    }
+}
