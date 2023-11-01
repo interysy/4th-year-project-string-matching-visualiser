36e296dfb9e9ebb1ffb74ed4615e1d4c4098f7ea
interysy <2554268w@student.gla.ac.uk>
2023-11-01 18:11:35 +0000

    style(pseudocode-visualiser): style the block with syntax highlightinh
    related to #100

--- src/app/modules/string-matching-algorithm-visualiser/components/pseudocode-visualiser/pseudocode-visualiser.component.ts
+++ src/app/modules/string-matching-algorithm-visualiser/components/pseudocode-visualiser/pseudocode-visualiser.component.ts
@@ -1,41 +1,55 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild , OnInit } from '@angular/core';
 import { AlgorithmProgressService } from '../../services/algorithm-progress.service';
 import { PseudocodeParserService } from '../../services/pseudocode-parser.service';
+import * as Prism from 'prismjs';
+import 'prismjs/components/prism-typescript';
+import "prismjs/plugins/line-highlight/prism-line-highlight";
+import "prismjs/plugins/toolbar/prism-toolbar";
+import "prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard";
+import "prismjs/plugins/match-braces/prism-match-braces";
+
 
 @Component({
   selector: 'app-pseudocode-visualiser',
   templateUrl: './pseudocode-visualiser.component.html',
-  styleUrls: ['./pseudocode-visualiser.component.css']
+  styleUrls: ['./pseudocode-visualiser.component.css',]
 })
-export class PseudocodeVisualiserComponent {
+export class PseudocodeVisualiserComponent implements OnInit {
 
-  lastLine : HTMLElement | null = null;
-  @ViewChild('pseudocodeVisualiserDiv') divToDisplay:ElementRef;
+  @ViewChild('pseudocodePreElement') private pseudocodePreElement:ElementRef;
+  @ViewChild('pseudocodeElement') private pseudocodeElement:ElementRef;
+  private language = "typescript";
+  private pseudocode: string;
+  protected formattedPseudocode: string;
+
 
   constructor(private readonly algorithmProgressService : AlgorithmProgressService,
               private readonly pseudocodeParserService : PseudocodeParserService) {
 
-    this.pseudocodeParserService.getAlgorithmPseudocode(this.algorithmProgressService.algorithmNameGetter).subscribe((pseudocode) => {
-
-      const splittedPseudocode = pseudocode.split("\n");
-      splittedPseudocode.forEach((line , index) => {
-        this.divToDisplay.nativeElement.insertAdjacentHTML('beforeend', '<p id="line' + index + '">' + line + '</p>');
-      })
-    });
-
     this.algorithmProgressService.notifier.subscribe((_) => {
-      this.highlightLine();
+      this.highlightLine(this.algorithmProgressService.pseudocodeLine);
     });
   }
 
-  highlightLine() {
-    if (this.lastLine) this.lastLine.style.backgroundColor = "";
+  ngOnInit(): void {
+    this.createButtons();
 
-    const lineToHighlight = document.getElementById("line" + this.algorithmProgressService.pseudocodeLine);
-    if (lineToHighlight) {
-      lineToHighlight.style.backgroundColor = "green";
-      this.lastLine = lineToHighlight;
-    }
+    this.pseudocodeParserService.getAlgorithmPseudocode(this.algorithmProgressService.algorithmNameGetter).subscribe((pseudocode) => {
+      this.pseudocode =  "\n" + pseudocode.trim();
+      this.formattedPseudocode = Prism.highlight(this.pseudocode, Prism.languages[this.language] , this.language);
+    });
   }
 
+  private createButtons() {
+    Prism.plugins["toolbar"].registerButton('language',
+      {
+        text: this.language
+      }
+    );
+  }
+
+  private highlightLine(lineNumber : number) {
+    this.pseudocodePreElement?.nativeElement.setAttribute('data-line', lineNumber.toString());
+    Prism.highlightElement(this.pseudocodeElement.nativeElement);
+  }
 }
