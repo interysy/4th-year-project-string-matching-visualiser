2ba18abb277eb621e7ac56950f04abb5a0d68cf8
interysy <2554268w@student.gla.ac.uk>
2023-11-05 14:31:10 +0000

    feat(p5js-draw-service): change size of animation based on screen size
    changes:
    - add function that changes canvas size
    - add function that determines size of plot (not working for the moment)

--- src/app/modules/string-matching-algorithm-visualiser/services/p5js-draw.service.ts
+++ src/app/modules/string-matching-algorithm-visualiser/services/p5js-draw.service.ts
@@ -12,7 +12,7 @@ import { Subject } from 'rxjs';
 })
 export class P5jsDrawService extends P5JSInvoker {
 
-  private readonly SquareSideSize = 20;
+  private squareSideSize = 30;
   private readonly DefaultFill  = MatchingAlgorithmColourConstants.DEFAULT;
   private readonly RectangleOffset = 5;
   private readonly SingleCharTextOffset = 10;
@@ -37,6 +37,7 @@ export class P5jsDrawService extends P5JSInvoker {
   }
 
   draw(p : p5) {
+    console.log("drawing in ", this.squareSideSize);
     if (this.step && this.algorithm) {
       this.algorithm.draw(p , this.step);
     }
@@ -58,7 +59,7 @@ export class P5jsDrawService extends P5JSInvoker {
   }
 
   workOutTextWidth(textLength : number) : void {
-    this.textWidth = textLength * this.SquareSideSize;
+    this.textWidth = textLength * this.squareSideSize;
   }
 
   centraliseDrawing(p : p5, canvasWidth : number , canvasHeight : number) : void {
@@ -78,13 +79,14 @@ export class P5jsDrawService extends P5JSInvoker {
   drawTextAndPattern(p : p5 ,textLettersToDraw : Letter[] , patternLettersToDraw : Letter[] , patternOffset : number) {
     if (p) {
 
-      const graphicalOffset = patternOffset * this.SquareSideSize;
+      const graphicalOffset = patternOffset * this.squareSideSize;
       this.drawText(p , textLettersToDraw);
       this.drawPattern(p, patternLettersToDraw , graphicalOffset);
     }
   }
 
   private drawText(p : p5, lettersToDraw : Letter[]) {
+    console.log("drawing in ", this.squareSideSize);
       lettersToDraw.forEach(letterObject => {
         let y = 0;
         const index = letterObject.index;
@@ -94,15 +96,15 @@ export class P5jsDrawService extends P5JSInvoker {
         const textOffset = (index > 9) ? this.DoubleCharTextOffset : this.SingleCharTextOffset;
 
         if (p) {
-          p.text(index , index * this.SquareSideSize + textOffset , y);
+          p.text(index , index * this.squareSideSize + textOffset , y);
           y = y + 10;
           p.fill(colour.toString());
           p.strokeWeight(strokeWeight);
-          p.rect(index * this.SquareSideSize + this.RectangleOffset , y , this.SquareSideSize , this.SquareSideSize);
+          p.rect(index * this.squareSideSize + this.RectangleOffset , y , this.squareSideSize , this.squareSideSize);
           y = y + 15;
           p.fill("#000000");
           p.strokeWeight(1);
-          p.text(letter , index * this.SquareSideSize  + textOffset , y);
+          p.text(letter , index * this.squareSideSize  + textOffset , y);
         }
       });
 
@@ -121,11 +123,11 @@ export class P5jsDrawService extends P5JSInvoker {
         if (p) {
           p.fill(colour.toString());
           p.strokeWeight(strokeWeight);
-          p.rect(index * this.SquareSideSize + this.RectangleOffset  + offset , y , this.SquareSideSize , this.SquareSideSize);
+          p.rect(index * this.squareSideSize + this.RectangleOffset  + offset , y , this.squareSideSize , this.squareSideSize);
           y = y + 15;
           p.fill("#000000");
           p.strokeWeight(1);
-          p.text(letter ,index * this.SquareSideSize + this.SingleCharTextOffset  + offset  , y);
+          p.text(letter ,index * this.squareSideSize + this.SingleCharTextOffset  + offset  , y);
         }
       })
   }
@@ -138,7 +140,7 @@ export class P5jsDrawService extends P5JSInvoker {
       y  += 15;
       p.fill("#000000");
       for (const [key, value] of Object.entries(lastOccuranceTable)) {
-        p.text(key + ':' + value + ',', this.SquareSideSize , y);
+        p.text(key + ':' + value + ',', this.squareSideSize , y);
         y += 10;
       }
       p.text('}', 0 , y);
@@ -146,9 +148,36 @@ export class P5jsDrawService extends P5JSInvoker {
   }
 
   private resizeCanvas(p : p5 , width : number , height : number) {
-    console.log("resize canvas")
-      p.resizeCanvas(width, height);
+    if (this.step) {
+      console.log("determine")
+      const newSquareSize = this.determineSquareSize(this.squareSideSize , this.step.lettersInText.length , width)
+      console.log("new size" , newSquareSize)
+      this.squareSideSize = newSquareSize;
+      console.log("updated " , this.squareSideSize)
+    }
+   console.log("again check" , this.squareSideSize)
+
+    p.resizeCanvas(width, height);
+
       //this.drawTextAndPattern(text , pattern);
+    }
+
+    private determineSquareSize(currentSquareSize : number , textLength : number , canvasWidth : number) : number {
+      let lengthInPixels = textLength * currentSquareSize;
+      if (lengthInPixels > canvasWidth) {
+        console.log("need decrease")
+        while (lengthInPixels > canvasWidth) {
+          currentSquareSize = currentSquareSize - 1;
+          console.log("new size" , currentSquareSize)
+          lengthInPixels = textLength * currentSquareSize;
+        }
+      } else {
+        while (lengthInPixels < canvasWidth) {
+          currentSquareSize = currentSquareSize + 1;
+          lengthInPixels = textLength * currentSquareSize;
+        }
+      }
+      return currentSquareSize;
     }
 
 }
