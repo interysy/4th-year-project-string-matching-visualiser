0609c792d51728004a36f54dcface53d758acfb6
interysy <2554268w@student.gla.ac.uk>
2023-11-23 10:16:23 +0000

    ci(karma): create a karma configuration to use firefox on my machine

---
+++ karma.conf.js
@@ -0,0 +1,12 @@
+module.exports = function (config) {
+    config.set({
+      frameworks: ['jasmine', '@angular-devkit/build-angular'],
+      plugins: [
+        require('karma-jasmine'),
+        require('karma-firefox-launcher'),
+        require('@angular-devkit/build-angular/plugins/karma'),
+      ],
+      browsers: ['Firefox'],
+      singleRun: true,
+    });
+  };
