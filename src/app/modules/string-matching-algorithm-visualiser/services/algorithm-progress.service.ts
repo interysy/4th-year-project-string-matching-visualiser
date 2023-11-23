a5ae6bf7a020f920b6adf470bbc3906ad0af857c
interysy <2554268w@student.gla.ac.uk>
2023-11-23 15:13:02 +0000

    docs(algorithm-progress): add documentation to service and refactor
    changes:
    - place all getters together
    - place all setters together
    - remove duplicated methods
    
    linked #105, #78

--- src/app/modules/string-matching-algorithm-visualiser/services/algorithm-progress.service.ts
+++ src/app/modules/string-matching-algorithm-visualiser/services/algorithm-progress.service.ts
@@ -4,20 +4,36 @@ import { StringMatchingAlgorithm } from '../models/algorithm.model';
 import { DrawStepDecorator } from '../models/drawer-step.decorator';
 import { StringMatchingAlgorithmToDraw } from '../models/algorithm-draw.model';
 
+/**
+ * @description The service is responsible for keeping track of the algorithm's progress. It also has functions so
+ * that components can modify the progress of the algorithm.
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class AlgorithmProgressService {
 
-  currentlyPlaying = false;
-  currentStep = 0;
-  notifier : Subject<number> = new Subject<number>();
-  amountOfSteps : number;
-  text : string;
-  pattern : string;
+  /**
+   * @description The notifier is used to notify the components that the algorithm's progress has changed.
+   * It allows the implementation of the observer pattern, where each component receives a notification upon change.
+   */
+  private notifier : Subject<number> = new Subject<number>();
+
+  private readonly DefaultSpeed = 1000;
+
+  private currentlyPlaying = false;
+  private currentStep = 0;
+  private amountOfSteps : number;
+  private text : string;
+  private pattern : string;
   private algorithm : StringMatchingAlgorithm;
-  private speed = 1000;
-  decoratedAlgorithm : DrawStepDecorator
+  private speed = this.DefaultSpeed;
+
+  /**
+   * @description The decorated algorithm is used to decorate the algorithm with decorators that allow the algorithm to be visualised with
+   * extra information when required.
+   */
+  private decoratedAlgorithm : DrawStepDecorator
 
   constructor() {
     this.notifier.subscribe((value : number) => {
@@ -25,7 +41,14 @@ export class AlgorithmProgressService {
     });
   }
 
-  public injectAlgorithm(algorithmToInject : { new (algorithmName : string): StringMatchingAlgorithm } , algorithmName : string , decorators : { new (decoratorName : StringMatchingAlgorithmToDraw): DrawStepDecorator }[] ) {
+  /**
+   * @description The function injects the algorithm into the service. It also resets the progress of the algorithm to allow new algorithm to run.
+   * @param algorithmToInject New algorithm to run
+   * @param algorithmName The algorithm name
+   * @param decorators The visual aspects to be drawn as an array of decorators
+   * @returns void
+   */
+  public injectAlgorithm(algorithmToInject : { new (algorithmName : string): StringMatchingAlgorithm } , algorithmName : string , decorators : { new (decoratorName : StringMatchingAlgorithmToDraw): DrawStepDecorator }[] ) : void {
 
     this.resetProgressService();
     this.algorithm = new algorithmToInject(algorithmName);
@@ -38,65 +61,125 @@ export class AlgorithmProgressService {
     }
   }
 
-  public executeAlgorithm() {
+  /**
+   * @description The function executes the algorithm and works out the steps.
+   * @returns void
+   */
+  public executeAlgorithm() : void {
     this.algorithm.workOutSteps(this.text, this.pattern);
     this.amountOfSteps = this.algorithm.stepsLengthGetter;
     this.notifier.next(0);
   }
 
-  public setTextAndPattern(text : string, pattern : string) {
+  /**
+   * @description The function sets the text and pattern and resets the algorithm's progress.
+   * @param text The text to be set
+   * @param pattern The pattern to be set
+   * @returns void
+   */
+  public setTextAndPattern(text : string, pattern : string) : void {
     this.text = text;
     this.pattern = pattern;
     this.algorithm.resetSteps();
     this.executeAlgorithm();
   }
 
+  /**
+   * @description The function resets the algorithm's progress.
+   */
   public reset() {
     this.currentlyPlaying = false;
     this.notifier.next(0);
   }
 
+  /**
+   * @description The function resets the algorithm's progress and any data related to it, so another algorithm can be executed.
+   */
   public resetProgressService() {
     this.currentlyPlaying = false;
     this.currentStep = 0;
     this.amountOfSteps = 0;
     this.text = "";
-    this.speed = 1000;
+    this.speed = this.DefaultSpeed;
   }
 
-  set setText(text : string) {
-    this.text = text;
-    this.algorithm.resetSteps();
-    this.executeAlgorithm();
-  }
-
-  set setPattern(pattern : string) {
-     this.pattern = pattern;
-     this.algorithm.resetSteps();
-     this.executeAlgorithm();
-  }
-
-  public moveToNextStep() {
+  /**
+   * @description The function moves the algorithm to the next step.
+   * Available only when not playing.
+   * @see play()
+   */
+  public moveToNextStep() : void {
     if (this.currentStep != this.amountOfSteps - 1) {
       this.notifier.next(this.currentStep + 1);
     }
   }
 
-  public moveToPreviousStep() {
+  /**
+   * @description The function moves the algorithm to the previous step.
+   * Available when not playing only.
+   * @see play()
+   */
+  public moveToPreviousStep() : void {
     if (this.currentStep > 0) {
       this.notifier.next(this.currentStep - 1);
     }
   }
 
-  public pause() {
+  /**
+   * @description The function pauses the algorithm.
+   * Available when playing only.
+   * @see play()
+   */
+  public pause() : void {
     this.currentlyPlaying = false;
+  }
+
+  /**
+   * @description The function plays the algorithm.
+   * Available when not playing only.
+   * @see pause()
+   */
+  async play() : Promise<void> {
+    this.currentlyPlaying = true;
+    while (this.currentStep != this.amountOfSteps-1 && this.currentlyPlaying) {
+      this.moveToNextStep();
+      await this.sleep(this.speed);
+    }
+    this.currentlyPlaying = false;
+  }
+
+  /**
+   *
+   * @param msec The time to sleep in milliseconds
+   * @returns Promise<void>
+   */
+  async sleep(msec: number) : Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, msec));
+  }
+
+  /**
+   * @description The function changes the speed of playback.
+   * @param speed The new speed of playback
+   */
+  changeSpeedOfPlayback(speed : number) : void {
+    this.speed = speed;
+  }
+
+
+  get notifierGetter() {
+    return this.notifier;
+  }
+
+  get currentStepNumberGetter() {
+    return this.currentStep;
+  }
+
+  get amountOfStepsGetter() {
+    return this.amountOfSteps;
   }
 
   get stepGetter() {
     return this.algorithm.stepsGetter[this.currentStep];
-  }
-  get currentStepNumber() {
-    return this.currentStep;
   }
 
   get pseudocodeLine() {
@@ -111,11 +194,9 @@ export class AlgorithmProgressService {
     return this.algorithm.stepsGetter[this.currentStep].textIndex;
   }
 
-
   get command() {
     return this.algorithm.stepsGetter[this.currentStep].command;
   }
-
 
   get textLength() {
     return this.algorithm.textLengthGetter;
@@ -125,7 +206,7 @@ export class AlgorithmProgressService {
     return this.algorithm.patternLengthGetter;
   }
 
-  get additionalVariables() {
+  get additionalVariablesGetter() {
     return this.algorithm.stepsGetter[this.currentStep].additional;
   }
 
@@ -133,28 +214,31 @@ export class AlgorithmProgressService {
     return this.algorithm.algorithmNameGetter;
   }
 
-  async play() {
-    this.currentlyPlaying = true;
-    while (this.currentStep != this.amountOfSteps-1 && this.currentlyPlaying) {
-      this.moveToNextStep();
-      await this.sleep(this.speed);
-    }
-    this.currentlyPlaying = false;
+  get decoratedAlgorithmGetter() {
+    return this.decoratedAlgorithm;
   }
 
-  async sleep(msec: number) {
-    return new Promise(resolve => setTimeout(resolve, msec));
+  get currentlyPlayingGetter() {
+    return this.currentlyPlaying;
   }
 
-  changeSpeedOfPlayback(speed : number) {
-    this.speed = speed;
+  get speedGetter() {
+    return this.speed;
   }
 
-  set currentStepSetter(step : number) {
+  set textSetter(text : string) {
+    this.text = text;
+    this.algorithm.resetSteps();
+    this.executeAlgorithm();
+  }
+
+  set patternSetter(pattern : string) {
+     this.pattern = pattern;
+     this.algorithm.resetSteps();
+     this.executeAlgorithm();
+  }
+
+  set currentStepNumberSetter(step : number) {
     this.notifier.next(step);
-  }
-
-  public getAdditionalVariables() {
-    return this.algorithm.stepsGetter[this.currentStep].additional;
   }
 }
