68d5bfb21d31ae73d18f1ba3ff877c11d6f47671
interysy <2554268w@student.gla.ac.uk>
2023-11-23 11:26:07 +0000

    docs(pseudocode-parser): add component docs
    linked to #78

--- src/app/modules/string-matching-algorithm-visualiser/services/pseudocode-parser.service.ts
+++ src/app/modules/string-matching-algorithm-visualiser/services/pseudocode-parser.service.ts
@@ -2,14 +2,29 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from 'rxjs';
 
+/**
+ * @description The service is responsible for fetching the pseudocode of the algorithm from the assets folder.
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class PseudocodeParserService {
 
+  private readonly FileSuffix = "-pseudocode.txt"
+  private readonly AssetsPath = "../../../assets/"
+  private readonly ResponseType = "text" as "json"
+
+
+  /**
+   * @param http The HttpClient service is used to fetch the pseudocode from the assets folder.
+   */
   constructor(private readonly http : HttpClient) {}
 
+  /**
+   * @param algorithmName The name of the algorithm to fetch the pseudocode for. Used in the filename.
+   * @returns A future string containing the pseudocode of the algorithm.
+   */
   getAlgorithmPseudocode(algorithmName : string) : Observable<string> {
-    return this.http.get<string>("../../../assets/" + algorithmName + "-pseudocode.txt" , {responseType : "text" as "json"});
+    return this.http.get<string>(this.AssetsPath + algorithmName + this.FileSuffix , {responseType : this.ResponseType});
   }
 }
