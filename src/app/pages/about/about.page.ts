5adffd6fdc06c2087239bdc7fdbad0acb1699ab8
interysy <2554268w@student.gla.ac.uk>
2023-11-23 10:31:14 +0000

    test(about-page): fix test for about page

--- src/app/pages/about/about.page.ts
+++ src/app/pages/about/about.page.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faCompass } from '@fortawesome/free-solid-svg-icons';
 
+
+library.add(faCompass, faGithub, faLinkedin);
 @Component({
   selector: 'app-about',
   templateUrl: './about.page.html',
