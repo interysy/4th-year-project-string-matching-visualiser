98abf362bc6d92df0ec9d0696b62e86a7f976e71
interysy <2554268w@student.gla.ac.uk>
2023-12-21 18:29:55 +0100

    style(app): create 3 themes for the application
    themes made:
    - 2 dark mode variants
    - 1 light mode variant
    
    related to  #132
    related to  #120

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,42 @@ module.exports = {
     "./node_modules/tw-elements/dist/js/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      textColor : {
+        skin : {
+          base : "var(--color-text-base)",
+          secondary : "var(--color-text-secondary)",
+          tertiary : "var(--color-text-tertiary)",
+          github : "var(--color-text-github)",
+          icon : "var(--color-icon)",
+          "icon-hover" : "var(--color-icon-hover)",
+          "icon-disabled": "var(--color-icon-disabled)",
+        }
+      },
+      backgroundColor : {
+        skin : {
+          "fill-main" : "var(--color-fill-main)",
+          "fill-secondary" : "var(--color-fill-secondary)",
+          "fill-tertiary" : "var(--color-fill-tertiary)",
+          "fill-tertiary-hover" : "var(--color-fill-tertiary-hover)",
+          "fill-quaternary" : "var(--color-fill-quaternary)",
+        }
+      },
+      borderColor : {
+        skin : {
+          "icon-separator" : "var(--color-icon-seperator)",
+          "border-focus" : "var(--color-border-focus)",
+        }
+      },
+      ringColor : {
+        skin : {
+          "focus" : "var(--color-ring-focus)",
+        }
+      }
+    },
   },
-  darkMode: "class",
-  plugins: [require("tw-elements/dist/plugin.cjs")]
+  darkMode: 'class',
+  plugins: [
+    require("tw-elements/dist/plugin.cjs"),
+  ]
 }
