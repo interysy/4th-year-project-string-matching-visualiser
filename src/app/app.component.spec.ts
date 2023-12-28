6e86b5ac8e22d622da4dde813aec1b2e392b853c
interysy <2554268w@student.gla.ac.uk>
2023-12-28 18:54:08 +0100

    test(app-component): add tests to app component
    these tests are mainly checking the theme gets change where required
    
    related to #108

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,17 +1,59 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ThemeSelectorService } from './modules/string-matching-algorithm-visualiser/services/theme-selector.service';
+import { Subject } from 'rxjs';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  let app: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  const mockedThemeChangedObserver$ = new Subject<string>();
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
+    declarations: [AppComponent],
+    providers: [
+      { provide: ThemeSelectorService, useClass : class {
+        themeChangedSubscriberGetter = mockedThemeChangedObserver$
+        currentThemeGetter = "base"
+      } }
+    ]
+  })});
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+
+  it("should create the app", () => {
     expect(app).toBeTruthy();
   });
+
+  it("should create themingDiv overlooking DOM" , () => {
+    expect(app.themingDivElement).toBeTruthy();
+  });
+
+  it('should apply the initial theme from the service', () => {
+
+    mockedThemeChangedObserver$.next("base")
+    fixture.detectChanges();
+
+    expect(app.currentTheme).toBe("base");
+    expect(app.themingDivElement.nativeElement.classList.contains("base")).toBe(true);
+  });
+
+  it('should update theme on theme change', fakeAsync(() => {
+
+    mockedThemeChangedObserver$.next("new-theme");
+
+    fixture.detectChanges();
+    tick();
+
+    expect(app.currentTheme).toBe("new-theme");
+    expect(app.themingDivElement.nativeElement.classList.contains("new-theme")).toBe(true);
+  }));
 
 });
