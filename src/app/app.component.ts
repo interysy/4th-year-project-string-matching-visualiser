932999efa481eebe0b85011626ecece49115b22b
interysy <2554268w@student.gla.ac.uk>
2023-12-28 18:52:39 +0100

    docs(app-component): add documentation to app componenyt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,40 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ThemeSelectorService } from './modules/string-matching-algorithm-visualiser/services/theme-selector.service';
 
+/**
+ * @description
+ * Entry point to the application. This element is a div that overlooks the entire DOM. It is used to apply a theme to the whole app.
+*/
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  /**
+   * @description Title of the application
+   */
   title = 'String Matching Algorithms Visualiser';
 
 
+  /**
+   * @description Reference to the div element that will be used to apply a theme to the whole app. The div
+   * overlooks the entire DOM.
+   */
   @ViewChild('themingDiv', {static: true})
   themingDivElement: ElementRef<HTMLDivElement>;
+
+  /**
+   * @description The current theme of the application, the actual class name applied to the div.
+   */
   currentTheme : string;
 
 
+  /**
+   * @description Constructor. Injects the ThemeSelectorService from the StringMatchingVisualiserModule that will be used to change theme.
+   * @param themeSelectorService
+   */
   constructor(private readonly themeSelectorService: ThemeSelectorService) {
     this.currentTheme = themeSelectorService.currentThemeGetter;
 
@@ -22,7 +42,5 @@ export class AppComponent {
       this.themingDivElement.nativeElement.classList.replace(this.currentTheme, newTheme);
       this.currentTheme = newTheme;
     });
-
   }
-
 }
