d6f2ca85a7edfd15881d362c90e40c7e835979e9
interysy <2554268w@student.gla.ac.uk>
2023-12-29 13:59:28 +0100

    refactor(option-service): change variable names to fit style guide

--- src/app/modules/string-matching-algorithm-visualiser/services/option.service.ts
+++ src/app/modules/string-matching-algorithm-visualiser/services/option.service.ts
@@ -2,86 +2,93 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class OptionService {
 
+  public readonly DefaultText = "The fox jumped over the lazy dog";
+  public readonly DefaultPattern = "lazy";
 
-  private $textChanged: Subject<string> = new Subject<string>();
-  private $patternChanged : Subject<string> = new Subject<string>();
-  private $smoothAnimationsChanged : Subject<boolean> = new Subject<boolean>();
-  private $preProcessingStepsChanged : Subject<boolean> = new Subject<boolean>();
-  private $showLegendChanged : Subject<boolean> = new Subject<boolean>();
+  private _textChanged$ : Subject<string> = new Subject<string>();
+  private _patternChanged$ : Subject<string> = new Subject<string>();
+  private _smoothAnimationsChanged$ : Subject<boolean> = new Subject<boolean>();
+  private _preProcessingStepsChanged$ : Subject<boolean> = new Subject<boolean>();
+  private _showLegendChanged$ : Subject<boolean> = new Subject<boolean>();
 
-  private text = "The fox jumped over the lazy dog";
-  private pattern = "lazy";
-  private smoothAnimations = false;
-  private preProcessingSteps = true;
-  private showLegend = false;
+  private _text : string;
+  private _pattern : string;
+  private _smoothAnimations = false;
+  private _preProcessingSteps = true;
+  private _showLegend = false;
+
+  constructor() {
+    this._text = this.DefaultText;
+    this._pattern = this.DefaultPattern;
+  }
 
   get textChangedSubscriberGetter() : Subject<string> {
-    return this.$textChanged;
+    return this._textChanged$;
   }
 
   get patternChangedSubscriberGetter() : Subject<string> {
-    return this.$patternChanged;
+    return this._patternChanged$;
   }
 
   get smoothAnimationsChangedSubscriberGetter() : Subject<boolean> {
-    return this.$smoothAnimationsChanged;
+    return this._smoothAnimationsChanged$;
   }
 
   get preProcessingStepsChangedSubscriberGetter() : Subject<boolean> {
-    return this.$preProcessingStepsChanged;
+    return this._preProcessingStepsChanged$;
   }
 
   get showLegendChangedSubscriberGetter() : Subject<boolean> {
-    return this.$showLegendChanged;
+    return this._showLegendChanged$;
   }
 
   set textSetter(text : string) {
-    this.text = text;
-    this.$textChanged.next(this.text);
+    this._text = text;
+    this._textChanged$.next(this._text);
   }
 
   set patternSetter(pattern : string) {
-    this.pattern = pattern;
-    this.$patternChanged.next(this.pattern);
+    this._pattern = pattern;
+    this._patternChanged$.next(this._pattern);
   }
 
   set smoothAnimationsSetter(smoothAnimations : boolean) {
-    this.smoothAnimations = smoothAnimations;
-    this.smoothAnimationsChangedSubscriberGetter.next(this.smoothAnimations);
+    this._smoothAnimations = smoothAnimations;
+    this.smoothAnimationsChangedSubscriberGetter.next(this._smoothAnimations);
   }
 
   set preProcessingStepsSetter(preProcessingSteps : boolean) {
-    this.preProcessingSteps = preProcessingSteps;
-    this.$preProcessingStepsChanged.next(this.preProcessingSteps);
+    this._preProcessingSteps = preProcessingSteps;
+    this._preProcessingStepsChanged$.next(this._preProcessingSteps);
   }
 
   set showLegendSetter(showLegend : boolean) {
-    this.showLegend = showLegend;
-    this.$showLegendChanged.next(this.showLegend);
+    this._showLegend = showLegend;
+    this._showLegendChanged$.next(this._showLegend);
   }
 
   get textGetter() : string {
-    return this.text;
+    return this._text;
   }
 
   get patternGetter() : string {
-    return this.pattern;
+    return this._pattern;
   }
 
   get smoothAnimationsGetter() : boolean {
-    return this.smoothAnimations;
+    return this._smoothAnimations;
   }
 
   get preProcessingStepsGetter() : boolean {
-    return this.preProcessingSteps;
+    return this._preProcessingSteps;
   }
 
   get showLegendGetter() : boolean {
-    return this.showLegend;
+    return this._showLegend;
   }
 
 }
