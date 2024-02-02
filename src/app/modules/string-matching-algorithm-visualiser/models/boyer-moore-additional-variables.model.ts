6b91904f7302d36ef12b459791cf2782b9d3bf38
interysy <2554268w@student.gla.ac.uk>
2024-02-02 17:29:57 +0000

    refactor(additional-variables): delete old additional variable files

--- src/app/modules/string-matching-algorithm-visualiser/models/boyer-moore-additional-variables.model.ts
+++
@@ -1,7 +0,0 @@
-import { AdditionalVariables } from "./additional-variables.model";
-
-export class BoyerMooreAdditionalVariables extends AdditionalVariables {
-    startingPoint : number;
-    lastOccurrenceTable : {[character : string] : number; };
-    lastOccurrenceToHighlight : string;
-}
--- src/app/modules/string-matching-algorithm-visualiser/models/brute-force-additional-variables.model.ts
+++
@@ -1,5 +0,0 @@
-import { AdditionalVariables } from "./additional-variables.model";
-
-export class BruteForceAdditionalVariables extends AdditionalVariables {
-    startingPoint : number;
-}
