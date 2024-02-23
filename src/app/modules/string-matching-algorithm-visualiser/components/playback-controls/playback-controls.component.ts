292fd729729e61a248692e7a789ac329d8e07ffd
interysy <2554268w@student.gla.ac.uk>
2024-02-23 20:37:29 +0000

    revert(keyboard-bindings): remove keyboard bindings
    done as these were messing around with the editing of options, due to time contraints these will be added as future work instead

--- src/app/modules/string-matching-algorithm-visualiser/components/playback-controls/playback-controls.component.ts
+++ src/app/modules/string-matching-algorithm-visualiser/components/playback-controls/playback-controls.component.ts
@@ -41,14 +41,6 @@ export class PlaybackControlsComponent implements OnDestroy {
    */
   subscriptions : Subscription[] = [];
 
-
-  private static ArrowUpKey = "ArrowUp";
-  private static ArrowDownKey = "ArrowDown";
-  private static ArrowRightKey = "ArrowRight";
-  private static ArrowLeftKey = "ArrowLeft";
-  private static SpaceKey = " ";
-  private static RKey = "r";
-
   /**
    * @description Initialises the component with the algorithm progress service and sets values from the service to the local variables for display.
    */
@@ -75,47 +67,6 @@ export class PlaybackControlsComponent implements OnDestroy {
     this.algorithmProgressService.play();
   }
 
-  /**
-   * @description Binding key cotnrols to keyboard keys
-   * @param $event The keypress event - used to get the key pressed
-   */
-  @HostListener('document:keydown', ['$event'])
-  handleKeyPress($event: KeyboardEvent) {
-    $event.preventDefault();
-
-    switch ($event.key) {
-      case PlaybackControlsComponent.ArrowRightKey:
-        if (!this.algorithmProgressService.currentlyPlayingGetter()) this.algorithmProgressService.moveToNextStep();
-        break;
-      case PlaybackControlsComponent.ArrowLeftKey:
-        if (!this.algorithmProgressService.currentlyPlayingGetter()) this.algorithmProgressService.moveToPreviousStep();
-        break;
-      case PlaybackControlsComponent.SpaceKey:
-        if (this.algorithmProgressService.currentlyPlayingGetter()) {
-          this.algorithmProgressService.pause();
-        } else {
-          this.algorithmProgressService.play();
-        }
-        break;
-      case PlaybackControlsComponent.RKey:
-        this.algorithmProgressService.resetProgress();
-        break;
-      case PlaybackControlsComponent.ArrowUpKey:
-        if (this.currentSpeed - 100 >= this.algorithmProgressService.MinimumSpeed) {
-          this.currentSpeed -= 100;
-          this.algorithmProgressService.changeSpeedOfPlayback(this.currentSpeed);
-        }
-        break;
-      case PlaybackControlsComponent.ArrowDownKey:
-        if (this.currentSpeed + 100 <= this.algorithmProgressService.MaximumSpeed) {
-          this.currentSpeed += 100;
-          this.algorithmProgressService.changeSpeedOfPlayback(this.currentSpeed);
-        }
-        break;
-      default:
-        break;
-    }
-  }
 
   /**
    * @description Moves to the next step in the algorithm.
