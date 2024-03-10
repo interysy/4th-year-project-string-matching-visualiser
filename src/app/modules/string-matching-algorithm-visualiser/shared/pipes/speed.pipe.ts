d216416d839ce0a6d664a887933bca1787b4097a
interysy <2554268w@student.gla.ac.uk>
2024-03-10 11:47:10 +0000

    docs(speed-pipe): adding description to pipe
    this pipe converts time between steps to a speed multiplier to display on the UI

--- src/app/modules/string-matching-algorithm-visualiser/shared/pipes/speed.pipe.ts
+++ src/app/modules/string-matching-algorithm-visualiser/shared/pipes/speed.pipe.ts
@@ -1,5 +1,10 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+/**
+ * @description A pipe used to convert the time between steps to speed of the algorithm as multiplier.
+ * We track speed by keeping track of time between steps.
+ * @see AlgorithmProgressService
+ */
 @Pipe({
   name: 'speedAsMultiplierPipe'
 })
