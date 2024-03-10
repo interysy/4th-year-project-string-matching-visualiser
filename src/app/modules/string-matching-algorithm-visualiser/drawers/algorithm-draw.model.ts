b6f50b3f7dfbb4da1ad68a5f9a94589fd87e0195
interysy <2554268w@student.gla.ac.uk>
2024-03-10 12:30:31 +0000

    docs(drawers): adding missing documentation to drawers

--- src/app/modules/string-matching-algorithm-visualiser/drawers/algorithm-draw.model.ts
+++ src/app/modules/string-matching-algorithm-visualiser/drawers/algorithm-draw.model.ts
@@ -1,6 +1,14 @@
 import { P5jsDrawClass } from "./p5js.drawer";
 
 
+/**
+ * @description An interface for string matching algorithm to implement. It creates the base layer to draw.
+ */
 export interface StringMatchingAlgorithmToDraw {
+
+    /**
+     * @description Method which is the last thing to draw on the canvas.
+     * @param obj The object on which canvas the function will draw on.
+     */
     draw(obj : P5jsDrawClass) : void;
 }
--- src/app/modules/string-matching-algorithm-visualiser/drawers/border-table.drawer.ts
+++ src/app/modules/string-matching-algorithm-visualiser/drawers/border-table.drawer.ts
@@ -3,13 +3,23 @@ import { DrawStepDecorator } from "../models/drawer-step.decorator";
 import { P5jsDrawClass } from "./p5js.drawer";
 
 
-
+/**
+ * @description A drawing layer, used to draw the border table for the KMP algorithm.
+ */
 export class BorderTableDrawer extends DrawStepDecorator {
 
+    /**
+     * @description Constructor initialises the layer and takes in an earlier one to call once finished.
+     * @param earlierDrawer Earlier layer.
+     */
     constructor(earlierDrawer : StringMatchingAlgorithmToDraw) {
         super(earlierDrawer);
     }
 
+    /**
+     * @description Function that actually draws the border table on the canvas. It calls an earlier layer when finished to continue drawing.
+     * @param drawingClass The object of which canvas we will draw on.
+     */
     override draw(drawingClass : P5jsDrawClass) : void {
         drawingClass.annotatePattern();
         this.earlierDrawer.draw(drawingClass);
